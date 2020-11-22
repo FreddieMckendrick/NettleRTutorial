7e69d5bfea65455417786d3f3e7463f30310a3f8
Freddie Mckendrick <freddiemckendrick@freddies-air.home>
2020-11-22 16:57:00 +0000

    Third commit

--- NettleRScript.R
+++ NettleRScript.R
@@ -3,6 +3,7 @@
 #url = 
 
 
+#SESSIONS 1 AND 2
 ### load packages ####
 library(readr)
 library(ggplot2)
@@ -15,10 +16,13 @@ str(weight)
 describe(weight)
 describeBy(weight$Weight, weight$Sex)
 
+#mean centering data 
+weight$Height.centred = weight$Height - mean(weight$Height)
+
 ### simple plots of the data ####
 plot(weight$Weight~weight$Height)
-ggplot(data = weight, aes(x= Height, y = Weight)) + geom_point() + theme_bw() 
-ggplot(data = weight, aes(x= Sex, y = Weight)) + geom_boxplot() + theme_bw() 
+(weightheight <- ggplot(data = weight, aes(x= Height, y = Weight)) + geom_point() + theme_bw())
+(weightsex <- ggplot(data = weight, aes(x= Sex, y = Weight)) + geom_boxplot() + theme_bw())
 
 ### linear model to look at the effect of sex on weight ####
 
@@ -32,3 +36,46 @@ weight$Sex2=relevel(weight$Sex, ref="Male")
 model2 <- lm(Weight~Sex2, data = weight)
 summary(model2)
 
+#model using continuous variable as the predictor 
+#using height to predict weight
+model3 <- lm(Weight ~ Height, data = weight)
+summary(model3)
+
+#add lm line on to previous graph
+(lmweightheight <- ggplot(data = weight, aes(x= Height, y = Weight)) + geom_point() + theme_bw() + geom_smooth(method = 'lm', data = model3))
+
+#multivariate analysis 
+#how does weight respond to both sex and height
+#height is mean centered
+#How much more does a man weigh than a woman on average when the man and the woman in question are the same height?
+model4 <- lm(Weight ~ Sex + Height.centred, data = weight)
+summary(model4)
+
+#check distribution of residuals 
+#residuals should generally be normally distributed 
+hist(model1$residuals)
+hist(model2$residuals)
+hist(model3$residuals)
+hist(model4$residuals)
+
+#for categorical variables the variance should be similar for all categories = homoscedaticity 
+#if these residuals are not similar in variance you get heteroscedascity
+plot(model4$residuals ~ weight$Sex)
+
+#for continuous variables you can plot the residuals againt the fitted values
+#fitted values = values of the linear predictor
+#residuals = differences between the fitted values and the actual values
+#should be an absence of overall pattern
+#hopegully equally variabel around 0
+plot(model3$residuals~model3$fitted.values)
+
+
+
+
+#SESSION 3
+
+### Load Data set ####
+stars <- read.csv("Fun with R/star.data.csv")
+head(stars)
+summary(stars)
+str(stars)
